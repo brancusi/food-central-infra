b052c64337a58c3496f76a9f9bc1828086aec94e
Aram Zadikian <brancusi@gmail.com>
2016-01-28 09:00:33 -0800

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+#--------------------------------------------------------------
+# AWS settings
+#--------------------------------------------------------------
+variable "access_key" {
+    description = "Please enter your AWS access key"
+}
+
+variable "secret_key" {
+    description = "Please enter your AWS secret key"
+}
+
+variable "region" {
+    description = "Please enter your desired region"
+}
